f505c1fb5e071352d4c0876ec33a9aa2067a12ce
merioane2020 <113840929+merioane2020@users.noreply.github.com>
2024-11-30 18:45:22 +0100

    Update WRDYAkey.js

--- WRDYAkey.js
+++ WRDYAkey.js
@@ -3,7 +3,7 @@
 loopy() 
  function loopy() { 
  var sWord ="mrwanxX" 
- while (sWord != "IOSMRWANXPANLE") {// Pass
+ while (sWord != "@mrwanxios") {// Pass
  sWord = prompt("𝘌𝘯𝘵𝘦𝘳 𝘛𝘩𝘦 𝘒𝘦𝘺 :")  
  } 
  
